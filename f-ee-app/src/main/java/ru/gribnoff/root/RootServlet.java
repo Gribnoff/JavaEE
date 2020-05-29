143c1e02a465bdd0af89c0a26b4b0fe58c7131d1
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-05-29 15:25:39 +0300

    Added RootServlet. Added shop link.
    Root page contains links to different projects/tasks.

---
+++ f-ee-app/src/main/java/ru/gribnoff/root/RootServlet.java
@@ -0,0 +1,20 @@
+package ru.gribnoff.root;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "RootServlet", urlPatterns = "")
+public class RootServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("headerText", "Root");
+		getServletContext().getRequestDispatcher("/util/header").include(request, response);
+
+		response.getWriter().printf("<a href=\"/f-ee-app/shop/main\">%s</a><br>", "Shop");
+	}
+}
+
