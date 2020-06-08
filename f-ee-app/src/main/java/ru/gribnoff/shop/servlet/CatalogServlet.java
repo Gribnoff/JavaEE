c4306558d0a04e8f5365c97d605144337abf6c73
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-06-08 20:28:37 +0300

    Added catalog servlet and page

--- f-ee-app/src/main/java/ru/gribnoff/shop/servlet/CatalogServlet.java
+++ f-ee-app/src/main/java/ru/gribnoff/shop/servlet/CatalogServlet.java
@@ -1,5 +1,7 @@
 package ru.gribnoff.shop.servlet;
 
+import ru.gribnoff.shop.entities.Product;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +11,20 @@ import java.io.IOException;
 
 @WebServlet(name = "CatalogServlet", urlPatterns = "/shop/catalog")
 public class CatalogServlet extends HttpServlet {
+	private String title = "Каталог";
+
+	@Override
+	public void init() {
+		for (int i = 0; i <= 10; i++) {
+			new Product("Товар №" + (i + 1), "Тестовый товар №" + (i + 1), 10 * (i + 1));
+		}
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("headerText", "Каталог");
-		getServletContext().getRequestDispatcher("/shop/util/menu").include(request, response);
-		getServletContext().getRequestDispatcher("/util/header").include(request, response);
+		request.setAttribute("title", title);
+		request.setAttribute("products", Product.getProducts());
+
+		getServletContext().getRequestDispatcher("/WEB-INF/views/catalog.jsp").forward(request, response);
 	}
 }
