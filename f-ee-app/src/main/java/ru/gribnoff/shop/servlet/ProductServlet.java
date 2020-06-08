49b1b99eff54fc9672ed77b0fca37661b24e6f78
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-06-08 20:25:36 +0300

    Added Product entity, servlet and page

--- f-ee-app/src/main/java/ru/gribnoff/shop/servlet/ProductServlet.java
+++ f-ee-app/src/main/java/ru/gribnoff/shop/servlet/ProductServlet.java
@@ -1,5 +1,7 @@
 package ru.gribnoff.shop.servlet;
 
+import ru.gribnoff.shop.entities.Product;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +11,15 @@ import java.io.IOException;
 
 @WebServlet(name = "ProductServlet", urlPatterns = "/shop/product")
 public class ProductServlet extends HttpServlet {
+	private String title;
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("headerText", "Товар");
-		getServletContext().getRequestDispatcher("/shop/util/menu").include(request, response);
-		getServletContext().getRequestDispatcher("/util/header").include(request, response);
+		Product product = Product.getProductByIdFromRequest(request);
+		title = product.getTitle();
+		request.setAttribute("title", title);
+		request.setAttribute("product", product);
+
+		getServletContext().getRequestDispatcher("/WEB-INF/views/product.jsp").forward(request, response);
 	}
 }
