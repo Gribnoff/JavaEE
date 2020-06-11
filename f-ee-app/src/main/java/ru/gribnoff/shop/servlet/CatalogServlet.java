df0d400b2b6ec83f03668effd60dfb410cb00734
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-06-11 20:54:20 +0300

    Added ProductRepository, data moved to db

--- f-ee-app/src/main/java/ru/gribnoff/shop/servlet/CatalogServlet.java
+++ f-ee-app/src/main/java/ru/gribnoff/shop/servlet/CatalogServlet.java
@@ -1,6 +1,8 @@
 package ru.gribnoff.shop.servlet;
 
-import ru.gribnoff.shop.entities.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.gribnoff.shop.repository.ProductRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,22 +10,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+
+import static ru.gribnoff.shop.listener.AppBootstrapListener.PRODUCT_REPOSITORY;
 
 @WebServlet(name = "CatalogServlet", urlPatterns = "/shop/catalog")
 public class CatalogServlet extends HttpServlet {
 	private String title = "Каталог";
+	private static ProductRepository productRepository;
+
+	private static final Logger logger = LoggerFactory.getLogger(CatalogServlet.class);
 
 	@Override
-	public void init() {
-		for (int i = 0; i <= 10; i++) {
-			new Product("Товар №" + (i + 1), "Тестовый товар №" + (i + 1), 10 * (i + 1));
+	public void init() throws ServletException {
+		productRepository = (ProductRepository) getServletContext().getAttribute(PRODUCT_REPOSITORY);
+		if (productRepository == null) {
+			throw new ServletException("Product repository not initialized");
 		}
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("title", title);
-		request.setAttribute("products", Product.getProducts());
+
+		try {
+			request.setAttribute("products", productRepository.findAll());
+		} catch (SQLException throwables) {
+			logger.warn("Nothing found!");
+		}
 
 		getServletContext().getRequestDispatcher("/WEB-INF/views/catalog.jsp").forward(request, response);
 	}
--- f-ee-app/src/main/java/ru/gribnoff/shop/servlet/ProductServlet.java
+++ f-ee-app/src/main/java/ru/gribnoff/shop/servlet/ProductServlet.java
@@ -1,6 +1,9 @@
 package ru.gribnoff.shop.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.gribnoff.shop.entities.Product;
+import ru.gribnoff.shop.repository.ProductRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,18 +11,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+
+import static ru.gribnoff.shop.listener.AppBootstrapListener.PRODUCT_REPOSITORY;
 
 @WebServlet(name = "ProductServlet", urlPatterns = "/shop/product")
 public class ProductServlet extends HttpServlet {
 	private String title;
+	private static ProductRepository productRepository;
+
+	private static final Logger logger = LoggerFactory.getLogger(ProductServlet.class);
+
+	@Override
+	public void init() throws ServletException {
+		productRepository = (ProductRepository) getServletContext().getAttribute(PRODUCT_REPOSITORY);
+		if (productRepository == null) {
+			throw new ServletException("Product repository not initialized");
+		}
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Product product = Product.getProductByIdFromRequest(request);
-		title = product.getTitle();
-		request.setAttribute("title", title);
-		request.setAttribute("product", product);
 
+		Product product = null;
+		try {
+			product = productRepository.findById((int) Long.parseLong(request.getParameter("id")));
+		} catch (SQLException throwables) {
+			logger.warn("Product with id={} not found!", request.getParameter("id"));
+			getServletContext().getRequestDispatcher("ExceptionHandler").forward(request, response);
+		}
+
+		if (product != null) {
+			title = product.getTitle();
+			request.setAttribute("title", title);
+			request.setAttribute("product", product);
+		}
 		getServletContext().getRequestDispatcher("/WEB-INF/views/product.jsp").forward(request, response);
 	}
 }
