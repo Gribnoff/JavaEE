fcef15a56b2c3a93a23fd3ef2d980747707cab5f
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-07-05 00:16:41 +0300

    Tried some RS stuff

---
+++ f-ee-app/src/main/java/ru/gribnoff/shop/rest/ProductServiceRS.java
@@ -0,0 +1,33 @@
+package ru.gribnoff.shop.rest;
+
+import ru.gribnoff.shop.entities.Product;
+
+import javax.ejb.Local;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Local
+@Path("/product")
+public interface ProductServiceRS {
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void save(Product product);
+
+	@DELETE
+	@Path("/{id}/delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void delete(@PathParam("id") Long id);
+
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	List<Product> findAll();
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	Product findById(@PathParam("id") Long id);
+
+}
---
+++ f-ee-app/src/main/java/ru/gribnoff/shop/rest/RSApp.java
@@ -0,0 +1,8 @@
+package ru.gribnoff.shop.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RSApp extends Application {
+}
